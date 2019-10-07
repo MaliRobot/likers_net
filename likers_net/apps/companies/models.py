709e7f210c188c2226f6b039c371a8d8146e2ec0
MaliRobot <milos.trifunovic@gmail.com>
2019-10-08 00:17:41 +0200

    add user company relation

--- likers_net/apps/companies/models.py
+++ likers_net/apps/companies/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from ..users.models import User
 
 # Create your models here.
 class Company(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     company_id = models.CharField(max_length=64, blank=True)
     company_name = models.CharField(max_length=64, blank=True)
     company_domain = models.TextField(max_length=255, blank=True)
