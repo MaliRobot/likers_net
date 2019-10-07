1baddfe45a24aa6170fc1a4575a0ae8ebcd1f9e7
MaliRobot <milos.trifunovic@gmail.com>
2019-10-08 00:23:01 +0200

    add posts

--- likers_net/apps/posts/models.py
+++ likers_net/apps/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ..users.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    lead = models.CharField(max_length=255)
+    text = RichTextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField()
+    language = models.CharField(max_length=3, default='eng')
+    date_published = models.DateTimeField('date published', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- likers_net/apps/users/models.py
+++ likers_net/apps/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ..companies.models import Company
+from ..posts.models import Post
 
 # Create your models here.
 class User(AbstractUser):
@@ -13,3 +14,12 @@ class User(AbstractUser):
         on_delete=models.CASCADE,
         related_name='users'
     )
+
+
+# class Like(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     created = models.DateTimeField(auto_now_add=True)
+#
+#     class Meta:
+#         unique_together = ("user", "post")
